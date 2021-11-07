72fd88baed401693b57ce8dfe9c04bce528d07ae
Daniel Kenyon-Jones <mail@dlkj.co.uk>
2021-11-07 16:55:05 +0000

    Add usb serial echoing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 //! Blinks the LED on a Adafruit Macropad RP2040 board
 //!
 //! This will blink on-board LED.
+//! Also echos USB serial input (minicom -b 115200 -o -D /dev/ttyACM0)
 #![no_std]
 #![no_main]
 
 use adafruit_macropad::{
+    hal,
     hal::{
         clocks::{init_clocks_and_plls, Clock},
         pac,
+        pac::interrupt,
         sio::Sio,
         watchdog::Watchdog,
     },
     Pins,
 };
+
+//use adafruit_macropad::hal::prelude::*;
+
 use cortex_m_rt::entry;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::rate::*;
+
 use panic_halt as _;
+
+use usb_device::{class_prelude::*, prelude::*};
+
+use usbd_serial::SerialPort;
+
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GD25Q64CS;
+
+static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+
+static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
 
 #[entry]
 fn main() -> ! {
@@ -41,6 +59,50 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet
+        USB_BUS = Some(usb_bus);
+    }
+
+    // Grab a reference to the USB Bus allocator. We are promising to the
+    // compiler not to take mutable access to this global variable whilst this
+    // reference exists!
+    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
+
+    // Set up the USB Communications Class Device driver
+    let serial = SerialPort::new(bus_ref);
+    unsafe {
+        USB_SERIAL = Some(serial);
+    }
+
+    // Create a USB device with a fake VID and PID
+    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet
+        USB_DEVICE = Some(usb_dev);
+    }
+
+    // Enable the USB interrupt
+    unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
+
+    //USB code now running
+
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
     let sio = Sio::new(pac.SIO);
@@ -57,5 +119,58 @@ fn main() -> ! {
         delay.delay_ms(1500);
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
+    }
+}
+
+/// This function is called whenever the USB Hardware generates an Interrupt
+/// Request.
+///
+/// We do all our USB work under interrupt, so the main thread can continue on
+/// knowing nothing about USB.
+#[allow(non_snake_case)]
+#[interrupt]
+fn USBCTRL_IRQ() {
+    use core::sync::atomic::{AtomicBool, Ordering};
+
+    /// Note whether we've already printed the "hello" message.
+    static SAID_HELLO: AtomicBool = AtomicBool::new(false);
+
+    // Grab the global objects. This is OK as we only access them under interrupt.
+    unsafe {
+        let usb_dev = USB_DEVICE.as_mut().unwrap();
+        let serial = USB_SERIAL.as_mut().unwrap();
+
+        // Say hello exactly once on start-up
+        if !SAID_HELLO.load(Ordering::Relaxed) {
+            SAID_HELLO.store(true, Ordering::Relaxed);
+            let _ = serial.write(b"Hello, World!\r\n");
+        }
+
+        // Poll the USB driver with all of our supported USB Classes
+        if usb_dev.poll(&mut [serial]) {
+            let mut buf = [0u8; 64];
+            match serial.read(&mut buf) {
+                Err(_e) => {
+                    // Do nothing
+                }
+                Ok(0) => {
+                    // Do nothing
+                }
+                Ok(count) => {
+                    // Convert to upper case
+                    buf.iter_mut().take(count).for_each(|b| {
+                        b.make_ascii_uppercase();
+                    });
+
+                    // Send back to the host
+                    let mut wr_ptr = &buf[..count];
+                    while !wr_ptr.is_empty() {
+                        let _ = serial.write(wr_ptr).map(|len| {
+                            wr_ptr = &wr_ptr[len..];
+                        });
+                    }
+                }
+            }
+        }
     }
 }
